0d17d8c6addfcbfdfa81a55bfe77c914ac9cc183
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-04-06 15:52:20 +0200

    Create function which calls the right operation

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let number1;
+let number2;
+let operator;
+
+/* Basic math operator functions */
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -12,4 +18,23 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 / num2;
+}
+
+/* Operate */
+
+function operate(number1, number2, operator) {
+  switch (operator) {
+    case "add":
+      return add(number1, number2);
+      break;
+    case "subtract":
+      return subtract(number1, number2);
+      break;
+    case "multiply":
+      return multiply(number1, number2);
+      break;
+    case "divide":
+      return divide(number1, number2);
+      break;
+  }
 }
