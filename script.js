fb483a0338dcef6f19fdbf1e87976ea2b8aad0cf
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-04-06 19:29:40 +0200

    Create all calculator html buttons

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 let number1;
 let number2;
 let operator;
-
-/* Create html buttons for each digit and the operations */
 const container = document.querySelector(".container");
 const display   = document.querySelector(".display");
 
-for (let i = 0; i < 10; i++) {
-  let button = document.createElement("button");
-  button.textContent = i;
-  container.appendChild(button);
+/* Function to create all buttons of the calculator */
+function createCalculatorButtons() {
+
+  // Create buttons 0-9
+  for (let i = 0; i < 10; i++) {
+    let button = document.createElement("button");
+    button.textContent = i;
+    container.appendChild(button);
+  }
+
+  // Create operator buttons
+  createButton("+");
+  createButton("-");
+  createButton("x");
+  createButton("÷");
+  createButton("+");
+  createButton("=");
+  createButton("clear");
 }
 
+/* Add button to the DOM */
+function createButton(text) {
+  let btn = document.createElement("button");
+  btn.textContent = text;
+  container.appendChild(btn);
+}
 
 /* Basic math operator functions */
-
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -32,7 +49,6 @@ function divide(num1, num2) {
 }
 
 /* Operate */
-
 function operate(number1, number2, operator) {
   switch (operator) {
     case "add":
@@ -49,3 +65,6 @@ function operate(number1, number2, operator) {
       break;
   }
 }
+
+/* Create html buttons for each digit and the operations */
+createCalculatorButtons();
