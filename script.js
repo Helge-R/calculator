1b10b1e7d99a614092948b7d3444ccc30e299558
Helge-R <109223097+Helge-R@users.noreply.github.com>
2023-04-06 16:02:55 +0200

    Create container for buttons and the buttons 0-9

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let number1;
 let number2;
 let operator;
 
+/* Create html buttons for each digit and the operations */
+const container = document.querySelector(".container");
+const display   = document.querySelector(".display");
+
+for (let i = 0; i < 10; i++) {
+  let button = document.createElement("button");
+  button.textContent = i;
+  container.appendChild(button);
+}
+
+
 /* Basic math operator functions */
 
 function add(num1, num2) {
